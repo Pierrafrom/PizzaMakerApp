7fefba95ee99842b1f4a09bd6463b52f0e802e51
Fromont Boissel <pierrefromont@outlook.fr>
2023-12-22 17:09:04 +0100

    add documentation and delete drink

--- src/main/java/com/pizzaMakerApp/style/SList.java
+++ src/main/java/com/pizzaMakerApp/style/SList.java
@@ -5,27 +5,51 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+/**
+ * A custom JList implementation with enhanced UI features.
+ * This class extends JList to provide additional styling and behavior like hover effects.
+ *
+ * @param <T> The type of elements this list will contain.
+ */
 public class SList<T> extends JList<T> {
 
+    // Index of the list item currently being hovered over by the mouse.
     private int hoveredIndex = -1;
-    public SList() {
 
+    /**
+     * Default constructor for SList.
+     */
+    public SList() {
+        super();
     }
 
+    /**
+     * Constructor for SList that takes a ListModel.
+     * Sets up the list with custom rendering, fonts, colors, and mouse interaction behavior.
+     *
+     * @param model The ListModel to use in this list.
+     */
     public SList(ListModel<T> model) {
         super(model);
+
+        // Set custom cell renderer to modify the look and feel of list items.
         setCellRenderer(new CustomListCellRenderer());
+
+        // Set the font and color scheme for the list from a predefined style class.
         setFont(Style.TEXT_FONT);
         setBackground(Style.BACKGROUND_COLOR);
         setForeground(Style.TEXT_COLOR);
 
-        // Supprimer la bordure
+        // Remove the border for a cleaner appearance.
         setBorder(null);
 
+        // Add mouse motion listener to handle hover behavior.
         addMouseMotionListener(new MouseAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
+                // Determine which list item is under the mouse.
                 int index = locationToIndex(e.getPoint());
+                // If the hovered item changes, update the hoveredIndex and repaint the list.
                 if (index != hoveredIndex) {
                     hoveredIndex = index;
                     repaint();
@@ -34,20 +58,26 @@ public class SList<T> extends JList<T> {
 
             @Override
             public void mouseExited(MouseEvent e) {
+                // Reset the hovered index when the mouse leaves the list and repaint.
                 hoveredIndex = -1;
                 repaint();
             }
         });
     }
 
+    /**
+     * Custom list cell renderer to change the appearance of list items based on state.
+     */
     private class CustomListCellRenderer extends DefaultListCellRenderer {
 
         @Override
         public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected,
                                                       boolean cellHasFocus) {
+            // Create a custom label for each list item.
             SLabel label = new SLabel(value.toString());
             label.setOpaque(true);
 
+            // Change the background and foreground of the label based on selection and hover state.
             if (isSelected) {
                 label.setBackground(Style.PRIMARY_COLOR);
                 label.setForeground(Style.TEXT_COLOR);
--- src/main/java/com/pizzaMakerApp/style/SSplitPane.java
+++ src/main/java/com/pizzaMakerApp/style/SSplitPane.java
@@ -8,25 +8,39 @@ import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
+/**
+ * A customized split pane class that extends JSplitPane.
+ * This class provides a styled version of the standard JSplitPane with custom divider and color.
+ */
 public class SSplitPane extends JSplitPane {
 
+    /**
+     * Constructor for SSplitPane.
+     * Applies custom styles and behaviors to the split pane.
+     */
     public SSplitPane() {
-        // apply styles
+        // Set the background color of the split pane from the Style class.
         this.setBackground(Style.NEUTRAL_COLOR);
+
+        // Set the size of the divider in the split pane.
         this.setDividerSize(3);
+
+        // Remove the border for a cleaner look.
         this.setBorder(null);
 
-        // divider customisation
+        // Customize the divider of the split pane.
         this.setUI(new BasicSplitPaneUI() {
             @Override
             public BasicSplitPaneDivider createDefaultDivider() {
                 return new BasicSplitPaneDivider(this) {
                     @Override
                     public void setBorder(Border b) {
+                        // Override to prevent setting any border on the divider.
                     }
 
                     @Override
                     public void paint(Graphics g) {
+                        // Paint the divider with the specified background color from the Style class.
                         g.setColor(Style.BACKGROUND_COLOR);
                         g.fillRect(0, 0, getSize().width, getSize().height);
                         super.paint(g);
@@ -34,12 +48,15 @@ public class SSplitPane extends JSplitPane {
                 };
             }
         });
+
+        // Set the initial position of the divider to the middle of the split pane.
         this.setDividerLocation(0.5);
 
-        // creating a resizing listener in order to adjust the divider
+        // Add a component listener to adjust the divider location when the component is resized.
         this.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
+                // Keep the divider at the middle of the split pane during resizing.
                 setDividerLocation(0.5);
             }
         });
