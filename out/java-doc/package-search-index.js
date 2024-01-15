6f04f71e75d360b05980ec3b0e0ddf45e2b0c2dc
Pierre Fromont Boissel <118841244+Pierrafrom@users.noreply.github.com>
2024-01-16 00:09:02 +0100

    Delete out/java-doc directory

--- out/java-doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.pizzaMakerApp.config"},{"l":"com.pizzaMakerApp.controller"},{"l":"com.pizzaMakerApp.main"},{"l":"com.pizzaMakerApp.model"},{"l":"com.pizzaMakerApp.style"},{"l":"com.pizzaMakerApp.utils"},{"l":"com.pizzaMakerApp.view"}];updateSearchResults();
